18e6035f6c358224501b07394e05084c12a35abc
aboosam532002-source <aboosam532002@gmail.com>
2025-12-16 23:59:43 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,5 @@
-module.exports = async (req, res) => {
-
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  console.log("REQ BODY:", req.body);
-
-  const { phone } = req.body || {};
-
-  if (!phone) {
-    return res.status(400).json({
-      success: false,
-      error: "Phone is required"
-    });
-  }
-
-  // مؤقتاً بدون TeleSign
-  return res.status(200).json({
-    success: true,
-    msg: "send-code reached"
+module.exports = (req, res) => {
+  res.status(200).json({
+    status: "API root working"
   });
 };
