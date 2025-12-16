05a418261bde903f7f5be4ad68e579b790ffc269
aboosam532002-source <aboosam532002@gmail.com>
2025-12-16 23:33:52 +0200

    Update send-code.js

--- api/send-code.js
+++ api/send-code.js
@@ -1,10 +1,6 @@
 module.exports = async (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: "API reached",
-    env: {
-      customerId: !!process.env.TELESIGN_CUSTOMER_ID,
-      apiKey: !!process.env.TELESIGN_API_KEY
-    }
+  res.status(200).json({
+    ok: true,
+    msg: "send-code reached"
   });
 };
