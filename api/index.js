2e402fac0ba0e5a74e91cf0099c6434fd82a4cde
aboosam532002-source <aboosam532002@gmail.com>
2025-12-16 16:20:02 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ status: "API is running" });
+  res.json({
+    status: "API is running"
+  });
 });
 
-module.exports = (req, res) => {
-  return app(req, res);
-};
-// force redeploy
+module.exports = app;
